e1d48f1658ea8fb7653c5315a9c32661082bd227
Nic Scobey <nicscobey@gmail.com>
2022-02-26 21:26:30 -0800

    js for loops

---
+++ JavaScript/arrays/forEach.js
@@ -0,0 +1,29 @@
+let myArray = [0,1,2,3,4]
+myArray.forEach(num => console.log(num % 2))
+
+// const getSum = () => {
+//     let sum = 0
+// 	myArray.forEach(num => {
+// 		sum += num
+//         console.log(sum)
+// 	})
+// 	return sum
+// }
+// getSum()
+
+// let sum = 0
+// let getSum = (num) => {
+//     sum += num
+// }
+
+// console.log(myArray.forEach(getSum))
+
+// let sum = 0
+// let getSum = (num) => {
+//     sum += num
+// }
+
+// myArray.forEach(num => getSum(num))
+
+// console.log(sum)
+
---
+++ JavaScript/arrays/for_in.js
@@ -0,0 +1,5 @@
+const myArray = [0,1,2,3,4]
+
+for (num in myArray) {
+    console.log(num)
+}
