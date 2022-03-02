884bd607a660cacc7a0a85b37cabcd534523aa8c
Nic Scobey <nicscobey@gmail.com>
2022-03-01 22:24:12 -0800

    this for

--- JavaScript/arrays/filter.js
+++ JavaScript/arrays/filter.js
@@ -1,14 +1,28 @@
 // creates an array of elements that match conditions from an existing array. does not modify original array
 
-let letters = ['a', 'a', 'b', 'c', 'd', 'a', 'e']
-const arrayOfA = letters.filter(letter => letter === 'a')
+// let letters = ['a', 'a', 'b', 'c', 'd', 'a', 'e']
+// const arrayOfA = letters.filter(letter => letter === 'a')
 
-console.log(letters) // returns ['a', 'a', 'b', 'c', 'd', 'a', 'e']
-console.log(arrayOfA) // returns ['a', 'a', 'a']
+// console.log(letters) // returns ['a', 'a', 'b', 'c', 'd', 'a', 'e']
+// console.log(arrayOfA) // returns ['a', 'a', 'a']
 
 
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-const greaterThanFive = numbers.filter(number => number > 5)
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+// const greaterThanFive = numbers.filter(number => number > 5)
 
-console.log(numbers) // returns [1, 2, 3, 4, 5, 6, 7, 8]
-console.log(greaterThanFive) // returns [ 6, 7, 8 ]
+// console.log(numbers) // returns [1, 2, 3, 4, 5, 6, 7, 8]
+// console.log(greaterThanFive) // returns [ 6, 7, 8 ]
+
+
+
+let animals = ["cat", "Chicken", "dog", "Pig", "Raccoon"]
+let returnValue = animals.filter(
+    animal => animal.includes("c")
+)
+console.log(returnValue)
+
+
+
+console.log(animals.filter(
+    animal => animal.includes("c")
+))
---
+++ JavaScript/arrays/sort.js
@@ -0,0 +1,3 @@
+let myArray = [0, 1, 10, 012, 11,  100, 23]
+myArray.sort()
+console.log(myArray)
